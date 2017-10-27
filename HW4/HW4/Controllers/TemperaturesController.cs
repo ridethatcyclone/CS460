fea0e6813ec11993b28db3d2e58d3df364180d11
Abby Horner <abbynhorner@gmail.com>
2017-10-26 18:53:31 -0700

    Created temperature conversion page

---
+++ HW4/HW4/Controllers/TemperaturesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW4.Controllers
+{
+    public class TemperaturesController : Controller
+    {
+        // GET: Temperatures
+        public ActionResult Conversion()
+        {
+            return View();
+        }
+    }
+}
