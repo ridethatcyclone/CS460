3fa840ec29ce2ac5a0b931806ff813bc611a1833
Abby Horner <abbynhorner@gmail.com>
2017-10-30 13:13:41 -0700

    Worked on guestbook; Should look into adding table

--- HW4/HW4/Controllers/GuestBookController.cs
+++ HW4/HW4/Controllers/GuestBookController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +9,26 @@ namespace HW4.Controllers
 {
     public class GuestBookController : Controller
     {
-        // GET: GuestBook
+        [HttpGet]
         public ActionResult Visitors()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Visitors(FormCollection form)
+        {
+
+            foreach (var value in form)
+            {
+                if (value == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ViewBag.FirstName = form["FirstName"];
+            ViewBag.LastName = form["LastName"];
+            ViewBag.Time = DateTime.Now;
+
             return View();
         }
     }
