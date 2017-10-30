22d6008b5731e6bfb3e05df66aeeece9d6c2c772
Abby Horner <abbynhorner@gmail.com>
2017-10-30 12:22:30 -0700

    Finished up temperature converter

--- HW4/HW4/Controllers/TemperaturesController.cs
+++ HW4/HW4/Controllers/TemperaturesController.cs
@@ -11,7 +11,53 @@ namespace HW4.Controllers
         // GET: Temperatures
         public ActionResult Conversion()
         {
+            string num = Request.QueryString["num"];
+            string type = Request.QueryString["type"];
+            double f, c, k;
+
+            ViewBag.num = num;
+            ViewBag.type = type;
+
+            if (!String.IsNullOrEmpty(num))
+            {
+                if (type.StartsWith("f"))
+                {
+                    ViewBag.f = Convert.ToDouble(num);
+                    ViewBag.c = (ViewBag.f - 32) * 5 / 9;
+                    ViewBag.k = ViewBag.c + 273.15;
+                    return View("Result");
+                }
+
+                else if (type[0] == 'c')
+                {
+                    ViewBag.c = Convert.ToDouble(num);
+                    ViewBag.f = (ViewBag.c * 9 / 5) + 32;
+                    ViewBag.k = ViewBag.c + 273.15;
+                    return View("Result");
+                }
+
+                else if (type[0] == 'k')
+                {
+                    ViewBag.k = Convert.ToDouble(num);
+                    ViewBag.c = ViewBag.k - 273.15;
+                    ViewBag.f = (ViewBag.c * 9 / 5) + 32;
+                    return View("Result");
+                }
+
+                else
+                {
+                    ViewBag.ErrorMessage("Invalid input");
+                    return View();
+                }
+
+            }
+            
+
             return View();
+            
         }
+
+        
+
     }
 }
