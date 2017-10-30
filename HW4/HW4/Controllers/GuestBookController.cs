e0cadfc6ef953a546f867cad71ef6b283508448c
Abby Horner <abbynhorner@gmail.com>
2017-10-30 12:33:54 -0700

    Began work on guestbook

---
+++ HW4/HW4/Controllers/GuestBookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW4.Controllers
+{
+    public class GuestBookController : Controller
+    {
+        // GET: GuestBook
+        public ActionResult Visitors()
+        {
+            return View();
+        }
+    }
+}
