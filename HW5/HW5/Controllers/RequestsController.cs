0b7a8d8607252ba252854d2295df543438249ba9
Abby Horner <abbynhorner@gmail.com>
2017-11-05 15:24:04 -0800

    tweaks

--- HW5/HW5/Controllers/RequestsController.cs
+++ HW5/HW5/Controllers/RequestsController.cs
@@ -11,27 +11,18 @@ namespace HW5.Controllers
     public class RequestsController : Controller
     {
         private RequestContext db = new RequestContext();
-        // GET: Requests
         public ActionResult Index()
         {
             return View(db.Requests.ToList());
         }
 
-        // GET: Requests/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Requests/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Requests/Create
         [HttpPost]
-        public ActionResult Create([Bind(Include ="FullName,DateOfBirth,CustomerID,StreetAddress,City,State,ZipCode,County")] Request request)
+        public ActionResult Create([Bind(Include = "FullName,DateOfBirth,CustomerID,StreetAddress,City,State,ZipCode,County")] Request request)
         {
             if (ModelState.IsValid)
             {
@@ -41,50 +32,6 @@ namespace HW5.Controllers
             }
 
             return View(request);
-        }
-
-        // GET: Requests/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Requests/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Requests/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Requests/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
