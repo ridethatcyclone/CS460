44baf5552a82427a04c378b35e4d229d066a191e
Abby Horner <abbynhorner@gmail.com>
2017-11-15 10:54:07 -0800

    Moved context to DAL folder; removed excess files and ensured database can be accessed by controller

--- HW6/HW6/Controllers/HomeController.cs
+++ HW6/HW6/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 
 namespace HW6.Controllers
 {
     public class HomeController : Controller
     {
+
+        //GET Index
         public ActionResult Index()
         {
-            return View();
+            using (ProductsContext db = new ProductsContext())
+            {
+                var products = db.Products;
+                return View(products.ToList());
+            }
         }
 
         public ActionResult About()
