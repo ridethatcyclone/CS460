7c0f53896bce10ca6b255d94df5f10b154d85ab7
Abby Horner <abbynhorner@gmail.com>
2017-11-19 12:37:00 -0800

    Got categories working; started on subcategories

--- HW6/HW6/Controllers/HomeController.cs
+++ HW6/HW6/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using HW6.Models.ViewModels;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -6,46 +7,79 @@ namespace HW6.Controllers
 {
     public class HomeController : Controller
     {
-        ProductsContext db = new ProductsContext();
+        //ProductsContext db = new ProductsContext();
+        MenuCategoriesVM menuViewModel = new MenuCategoriesVM();
 
         //GET Index
         public ActionResult Index()
         {
             /*
-            List<string> Categories = new List<string>();
-            for (int i = 1; i <= db.ProductCategories.Count(); i++)
+            using (ProductsContext db = new ProductsContext())
             {
-                Categories.Add(db.ProductCategories.Where(p => p.ProductCategoryID == i).Select(p => p.Name).ToString());
+                var products = db.Products;
+                return View(products.ToList());
             }
-            ViewData["Categories"] = new SelectList(Categories);
             */
-
-
 
             /*
-            ViewBag.Categories = new string[4];
+            var Cat = new string[db.ProductCategories.Count()];
             for (int i = 0; i < db.ProductCategories.Count(); i++)
             {
-                ViewBag.Categories[i] = db.ProductCategories.Select(p => p.Name).ToString();
+                Cat[i] = db.ProductCategories.Where(p => p.ProductCategoryID == i).Select(p => p.Name).FirstOrDefault().ToString();
             }
+            ViewBag.Categories = Cat;
+            */
+            using (ProductsContext db = new ProductsContext())
+            {
+                var Cat = new string[db.ProductCategories.Count()];
+                for (int i = 1; i <= db.ProductCategories.Count(); i++)
+                {
+                    var x = db.ProductCategories.Where(p => p.ProductCategoryID == i).Select(p => p.Name).FirstOrDefault().ToString();
+                    if (x != null) Cat[i - 1] = x;
+                }
+                ViewBag.Categories = Cat;
+                
+                for (int i = 1; i < db.ProductCategories.Count(); i++)
+                {
+                    IList<string> a = new List<string>();
+                    foreach (var item in db.ProductSubcategories)
+                    {
+                        if (db.ProductSubcategories.Select(p => p.ProductSubcategoryID).Equals(i))
+                        {
+                            a.Add(db.ProductSubcategories.Select(p => p.Name).FirstOrDefault().ToString());
+                        }
+                    }
+                    ViewData[i.ToString()] = a;
+                }
+
+                /*
+                 * SUBCATEGORIES SETUP:
+                 * [KEY: CATEGORY ID]
+                 * [VALUE: STRING ARRAY]
+                 * FIND NUMBER OF SUBCATEGORIES UNDER ONE IDNUMBER
+                 * RUN THROUGH LOOP
+                 * 
+                 * FOR INT I = 1; 1-4
+                 * IF SUBCATEGORY ID == I
+                 * ADD TO LIST VIEWBAG[I.TOSTRING]
+                 * 
+                 * /
+            }
+            
+            /*
+            var Subcat = new string[db.ProductSubcategories.Count()];
+            for (int i = 0; i < Subcat.Length; i++)
+            {
+                Subcat[i] = db.ProductSubcategories.Select(p => p.Name).ToString();
+            }
+            ViewBag.Subcategories = Subcat;
             */
 
-            
-            return View();
+                return View(menuViewModel);
+            }
+
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
---
+++ HW6/HW6/Models/ViewModels/MenuCategoriesVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW6.Models.ViewModels
+{
+    public class MenuCategoriesVM
+    {
+        public int ProductCategoryID { get; set; }
+        public string ProductCategoryName { get; set; }
+        public int ProductSubcategoryID { get; set; }
+        public string ProductSubcategoryName { get; set; }
+    }
+}
