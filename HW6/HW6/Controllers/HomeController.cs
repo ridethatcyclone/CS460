034346869ee8bea8b6934c716242c1246c16d41f
Abby Horner <abbynhorner@gmail.com>
2017-11-19 10:18:35 -0800

    working on populating navbar

--- HW6/HW6/Controllers/HomeController.cs
+++ HW6/HW6/Controllers/HomeController.cs
@@ -1,19 +1,37 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace HW6.Controllers
 {
     public class HomeController : Controller
     {
+        ProductsContext db = new ProductsContext();
 
         //GET Index
         public ActionResult Index()
         {
-            using (ProductsContext db = new ProductsContext())
+            /*
+            List<string> Categories = new List<string>();
+            for (int i = 1; i <= db.ProductCategories.Count(); i++)
             {
-                var products = db.Products;
-                return View(products.ToList());
+                Categories.Add(db.ProductCategories.Where(p => p.ProductCategoryID == i).Select(p => p.Name).ToString());
             }
+            ViewData["Categories"] = new SelectList(Categories);
+            */
+
+
+
+            /*
+            ViewBag.Categories = new string[4];
+            for (int i = 0; i < db.ProductCategories.Count(); i++)
+            {
+                ViewBag.Categories[i] = db.ProductCategories.Select(p => p.Name).ToString();
+            }
+            */
+
+            
+            return View();
         }
 
         public ActionResult About()
