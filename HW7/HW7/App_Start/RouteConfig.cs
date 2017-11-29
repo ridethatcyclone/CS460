40d31f1a5c25cde462424e18c1471a837f0b30e2
Abby Horner <abbynhorner@gmail.com>
2017-11-29 15:14:14 -0800

    Adding a custom route and prettying it up

--- HW7/HW7/App_Start/RouteConfig.cs
+++ HW7/HW7/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace HW7
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Search",
+                url: "search/",
+                defaults: new { controller = "SearchGiphy", action = "Search" }
+            );
+
         }
     }
 }
--- HW7/HW7/Scripts/script.js
+++ HW7/HW7/Scripts/script.js
@@ -9,7 +9,7 @@ function main()
             console.log("Nothing entered.");
         }
         else {
-            searchString = $("#sbar").val();
+            searchString = $("#sbar").val().replace(" ", "+");
             $("#sbar").val('');
             requestLink = "http://api.giphy.com/v1/gifs/search?q=" + searchString + "&api_key=" + key;
 
@@ -22,33 +22,19 @@ function main()
                 dataType: 'json',
                 success: function (response) {
                     console.log('success', response);
-                    //$.each(data, function (index, item) {
-                      //  $('#gifList').append('<p>' + item + '</p>');
-                    //});
-                    /*
-                    for (i = 0; i < 3; i++) {
-                        $('#gifList').append('<div class="row">')
-                        for (j = i + 1; j < i + 2; j++) {
-                            $('#gifList').append(
-                                '<div class="col-md-4"><img src=' + response.data[j-1].images.fixed_height.url + ' class="displayGif" ></div>'
-                            );
-                        }
-                        $('#gifList').append('</div>');
-                    }
-                    */
+                    
 
                     for (i = 0; i < 9; i++) {
                         if (i < 3) {
-                            $('#topRow').append('<div class="col-md-4"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
+                            $('#topRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
                         }
                         else if (i >= 3 && i < 6) {
-                            $('#middleRow').append('<div class="col-md-4"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
+                            $('#middleRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
                         }
                         else {
-                            $('#bottomRow').append('<div class="col-md-4"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
+                            $('#bottomRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
                         }
                     }
-                    //$('#gifList').append('<p>' + response.data[0].id + '</p>');
                 }
             })
         }
