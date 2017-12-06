6ae41ebbf943635e2566aec0b000a45a338a26d0
Abby Horner <abbynhorner@gmail.com>
2017-12-06 11:11:45 -0800

    finished

--- HW7/HW7/Scripts/script.js
+++ HW7/HW7/Scripts/script.js
@@ -9,12 +9,12 @@ function main()
             console.log("Nothing entered.");
         }
         else {
+            var animated = $("input[name='animated']:checked").val();
             searchString = $("#sbar").val().replace(" ", "+");
             $("#sbar").val('');
             requestLink = "http://api.giphy.com/v1/gifs/search?q=" + searchString + "&api_key=" + key;
 
             $(".gifRow").html(null);
-            console.log(requestLink);
 
             $.ajax({
                 type: 'GET',
@@ -23,18 +23,34 @@ function main()
                 success: function (response) {
                     console.log('success', response);
                     
-
-                    for (i = 0; i < 9; i++) {
-                        if (i < 3) {
-                            $('#topRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
-                        }
-                        else if (i >= 3 && i < 6) {
-                            $('#middleRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
-                        }
-                        else {
-                            $('#bottomRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
+                    if (animated == 'true') {
+                        for (i = 0; i < 9; i++) {
+                            if (i < 3) {
+                                $('#topRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
+                            }
+                            else if (i >= 3 && i < 6) {
+                                $('#middleRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
+                            }
+                            else {
+                                $('#bottomRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width.url + '" class="displayGif"></div>');
+                            }
                         }
                     }
+
+                    else {
+                        for (i = 0; i < 9; i++) {
+                            if (i < 3) {
+                                $('#topRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width_still.url + '" class="displayGif"></div>');
+                            }
+                            else if (i >= 3 && i < 6) {
+                                $('#middleRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width_still.url + '" class="displayGif"></div>');
+                            }
+                            else {
+                                $('#bottomRow').append('<div class="col-md-4 gifCol"><img src="' + response.data[i].images.fixed_width_still.url + '" class="displayGif"></div>');
+                            }
+                        }
+                    }
+                    
                 }
             })
         }
